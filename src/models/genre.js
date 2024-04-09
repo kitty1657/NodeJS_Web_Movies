4bfa8c58287bd312369b07be6e012f8842e2079c
bachoilabach <115871551+bachoilabach@users.noreply.github.com>
2024-04-09 21:18:36 +0700

    chore: add genreID

--- src/models/genre.js
+++ src/models/genre.js
@@ -9,6 +9,11 @@ module.exports = (sequelize, DataTypes) => {
 	}
 	Genre.init(
 		{
+			genreID: {
+				type: DataTypes.INTEGER,
+				primaryKey: true,
+				autoIncrement: true,
+			},
             name: DataTypes.STRING
         },
 		{
