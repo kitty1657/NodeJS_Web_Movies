5b71b8fe26e9f7de4df3b72f34259a4f8d290a83
bachoilabach <115871551+bachoilabach@users.noreply.github.com>
2024-04-09 21:19:58 +0700

    add: genre service

---
+++ src/services/genreService.js
@@ -0,0 +1,110 @@
+import genre from '../models/genre';
+import db from '../models/index';
+
+const getAllGenres = (genreID)=>{
+    return new Promise(async (resolve, reject) => {
+		try {
+			let genres = '';
+			if (genreID === 'ALL') {
+				genres = await db.genre.findAll();
+			}
+			if (genreID && genreID !== 'ALL') {
+				genres = await db.genre.findOne({
+					where: { genreID: genreID },
+				});
+			}
+			resolve(genres);
+		} catch (error) {
+			reject(error);
+		}
+	});
+}
+
+const createNewGenre = async (newGenreData) => {
+	return new Promise(async (resolve, reject) => {
+		try {
+			const existingGenre = await db.genre.findOne({
+				where: { name: newGenreData.name },
+			});
+
+			if (existingGenre) {
+				resolve({
+					errCode: 1,
+					ereMessage: 'Genre already exists',
+				});
+			} else {
+				const createdGenre = await db.genre.create(newGenreData);
+				resolve({
+					errCode: 0,
+					ereMessage: 'Create Genre Success',
+					createdGenre,
+				});
+			}
+		} catch (error) {
+			reject(error);
+		}
+	});
+};
+
+const editGenre = async (data) => {
+	return new Promise(async (resolve, reject) => {
+		try {
+			if (!data.genreID) {
+				resolve({
+					errCode: 2,
+					errMessage: 'Missing required parameters',
+				});
+			} else {
+				let genre = await db.genre.findOne({
+					where: { genreID: data.genreID },
+					raw: false,
+				});
+				if (genre) {
+					genre.name = data.name;
+                    console.log(data.name)
+					await genre.save();
+					resolve({
+						errCode: 0,
+						errMessage: 'Update Success',
+					});
+				} else {
+					resolve({
+						errCode: 1,
+						errMessage: `Genre isn't found`,
+					});
+				}
+			}
+		} catch (error) {
+			reject(error);
+		}
+	});
+};
+
+const deleteGenre = (genreID) => {
+	return new Promise(async(resolve, reject) => {
+		let genre = await db.genre.findOne({
+			where: {genreID: genreID}
+		})
+        console.log(genreID)
+		if(!genre){
+			resolve({
+				errCode: 2,
+				errMessage: `The genre isn't exist`
+			})
+		}
+		await db.genre.destroy({
+			where: {genreID: genreID}
+		})
+		resolve({
+			errCode: 0,
+			errMessage: 'Delete Success'
+		})
+	});
+};
+
+module.exports = {
+    getAllGenres: getAllGenres,
+    createNewGenre: createNewGenre,
+    editGenre: editGenre,
+    deleteGenre: deleteGenre
+}
