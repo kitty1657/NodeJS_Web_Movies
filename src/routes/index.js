99c5d5d55c62d7d65b3766191d226158a055060c
bachoilabach <115871551+bachoilabach@users.noreply.github.com>
2024-04-09 21:19:20 +0700

    add: route of actor, genre

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,30 @@
 import express from "express"
-import homeController from "../controllers/homeController";
 import userController from '../controllers/userController';
 import actorController from '../controllers/actorController';
-
+import genreController from '../controllers/genreController';
 const router = express.Router();
 
 const initWebRoutes = (app)=>{
-    router.get('/',homeController.getHomePage)
-    router.get('/crud',homeController.getCRUD)
     router.post('/api/login',userController.handleLogin)
+
+    // * User
     router.get('/api/get-all-users',userController.handleGetAllUsers)
     router.post('/api/create-new-user',userController.handleCreateNewUser)
     router.put('/api/edit-user',userController.handleEditUser)
     router.delete('/api/delete-user',userController.handleDeleteUser)
     router.get('api/user-role',userController.handleGetAllUserRole)
+
+    // * Actor
     router.get('/api/get-all-actors',actorController.handleGetAllActors)
+    router.post('/api/create-new-actor',actorController.handleCreateNewActor)
+    router.put('/api/edit-actor',actorController.handleEditActor)
+    router.delete('/api/delete-actor',actorController.handleDeleteActor)
+
+    // * Category
+    router.get('/api/get-all-genre',genreController.handleGetAllGenres)
+    router.post('/api/create-new-genre',genreController.handleCreateNewGenre)
+    router.put('/api/edit-genre',genreController.handleEditGenre)
+    router.delete('/api/delete-genre',genreController.handleDeleteGenre)
 
     return app.use('/',router);
 }
