ccca512fb7b1b12ad7f6b08345efc53332a69d49
bachoilabach <115871551+bachoilabach@users.noreply.github.com>
2024-05-12 16:45:43 +0700

    add: use cookie

--- src/index.js
+++ src/index.js
@@ -18,12 +18,8 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 
-// //test
-// createJWT();
-// let decodedData = verifyToken(
-// 	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiQmFjaCIsInBob25lIjoiMDM0NjMzMTk2OCIsImlhdCI6MTcwODc3NDAxNH0.cN-kZ8dxvP2aZKmMenhxW4yWE3Y6pEw4Tf-qAyj9tCI"
-// );
-// console.log(decodedData)
+// config cookie-parser
+app.use(cookieParser())
 
 viewEngine(app);
 initWebRoutes(app);
